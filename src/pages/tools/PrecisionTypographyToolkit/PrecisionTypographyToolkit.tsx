35ee0f2bc2c2b0b32cfaf561acc05ac446c980d5
Egil Eskilsson <egil@egileskilsson.se>
2025-12-30 12:13:02 +0100

    refactor(PrecisionTypographyToolkit): remove dev test components
    - Remove Button, Toggle, ThumbSlider test sections
    - Move component testing to StyleGuide
    - Keep core functionality (DropZone, MetricTable)
    - Comment out unused state for future features

--- src/pages/tools/PrecisionTypographyToolkit/PrecisionTypographyToolkit.tsx
+++ src/pages/tools/PrecisionTypographyToolkit/PrecisionTypographyToolkit.tsx
@@ -9,16 +9,7 @@ import { parseFontFile } from '@/utils/fontParser';
 import { MetricTable } from './MetricTable';
 import { useMediaQuery } from '@/hooks';
 import { queries } from '@/types';
-import { Button } from '@/components/forms/Button';
-import { SassIcon } from '@/assets/icons';
-import { SuccessIcon } from '@/assets/icons';
-import { WarningIcon } from '@/assets/icons';
-import { DangerIcon } from '@/assets/icons';
-import { InfoIcon } from '@/assets/icons';
-import { ButtonGroup } from '@/components/layout/ButtonGroup';
-import { Toggle } from '@/components/forms/Toggle';
 import { useState } from 'react';
-import { ThumbSlider } from '@/components/forms/ThumbSlider';
 import { Collapsible } from '@/components/ui/Collapsible';
 
 /**
@@ -26,6 +17,12 @@ import { Collapsible } from '@/components/ui/Collapsible';
  */
 const PrecisionTypographyToolkitContent = () => {
   const { state, dispatch } = useFontMetrics();
+  // const [toggles, setToggles] = useState({
+  //   kerning: false,
+  // });
+  // const [thumbSliders, setThumbSliders] = useState({
+  //   lineHeight: 1.2,
+  // });
 
   const isUnderBreakpoint = useMediaQuery(queries.isUpToTabletLarge);
 
@@ -46,20 +43,13 @@ const PrecisionTypographyToolkitContent = () => {
     }
   };
 
-  // DEV Start
-  const [toggles, setToggles] = useState({
-    feature1: false,
-    feature2: false,
-  });
+  // const handleToggleChange = (key: string) => (checked: boolean) => {
+  //   setToggles((prev) => ({ ...prev, [key]: checked }));
+  // };
 
-  const handleToggleChange = (key: string) => (checked: boolean) => {
-    setToggles((prev) => ({ ...prev, [key]: checked }));
-  };
-
-  const [slider1, setSlider1] = useState(1.2);
-  const [slider2, setSlider2] = useState(1.2);
-
-  // End DEV
+  // const handleThumbSliderChange = (key: string) => (value: number) => {
+  //   setThumbSliders((prev) => ({ ...prev, [key]: value }));
+  // };
 
   return (
     <>
@@ -86,112 +76,6 @@ const PrecisionTypographyToolkitContent = () => {
         marginTop="xl"
       >
         <MetricTable />
-      </Container>
-
-      <Container variant="boxed" marginTop="xl">
-        <ButtonGroup>
-          <Button variant="primary" icon={SassIcon}>
-            Primary
-          </Button>
-          <Button variant="secondary" icon={SassIcon}>
-            Secondary
-          </Button>
-          <Button variant="accent" icon={SassIcon}>
-            Accent
-          </Button>
-          <Button variant="ghost" icon={SassIcon}>
-            Ghost
-          </Button>
-          <Button variant="link">Link</Button>
-          <Button variant="success" icon={SuccessIcon}>
-            Success
-          </Button>
-          <Button variant="warning" icon={WarningIcon}>
-            Warning
-          </Button>
-          <Button variant="danger" icon={DangerIcon}>
-            Danger
-          </Button>
-          <Button variant="info" icon={InfoIcon}>
-            Info
-          </Button>
-          <Button variant="primary" icon={SassIcon} disabled>
-            Disabled
-          </Button>
-          <Button variant="ghost" icon={SassIcon} disabled>
-            Ghost
-          </Button>
-          <Button variant="link" disabled>
-            Link
-          </Button>
-        </ButtonGroup>
-      </Container>
-
-      <Container variant="boxed" marginTop="xl">
-        <ButtonGroup marginBottom="lg">
-          <Toggle
-            toggleId="toggle-1"
-            onChange={handleToggleChange('feature1')}
-            checked={toggles.feature1}
-          />
-          <Toggle
-            toggleId="toggle-2"
-            onChange={handleToggleChange('feature2')}
-            checked={toggles.feature2}
-            disabled
-          />
-        </ButtonGroup>
-
-        <ButtonGroup direction="column">
-          <ThumbSlider
-            inputId="slider-1"
-            min={1}
-            max={2}
-            step={0.05}
-            value={slider1}
-            onChange={setSlider1}
-            label={`value: ${slider1.toFixed(2)}`}
-          />
-          <ThumbSlider
-            inputId="slider-1"
-            min={1}
-            max={2}
-            step={0.05}
-            value={slider2}
-            onChange={setSlider2}
-            disabled
-            label="disabled"
-          />
-        </ButtonGroup>
-      </Container>
-      <Container variant="narrow" marginTop="xl">
-        <Collapsible
-          headingLevel={2}
-          headingSize="heading-4"
-          title="Collapsible Title"
-        >
-          <div className="flow-em">
-            <h4 className="heading-3">Lorem ipsum dolor sit amet</h4>
-            <p className="text-base">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
-              voluptatem corporis officia dolorum aut incidunt repellat iste quo
-              recusandae non, a delectus eligendi perferendis facere et iusto
-              cupiditate dicta animi!
-            </p>
-            <p className="text-base">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
-              accusantium laboriosam ipsum blanditiis voluptate sed quo officia,
-              maxime fugiat, sapiente exercitationem voluptatum, inventore
-              possimus aspernatur assumenda? Dolore perspiciatis ipsa unde.
-            </p>
-            <p className="text-base">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
-              accusantium laboriosam ipsum blanditiis voluptate sed quo officia,
-              maxime fugiat, sapiente exercitationem voluptatum, inventore
-              possimus aspernatur assumenda? Dolore perspiciatis ipsa unde.
-            </p>
-          </div>
-        </Collapsible>
       </Container>
     </>
   );
