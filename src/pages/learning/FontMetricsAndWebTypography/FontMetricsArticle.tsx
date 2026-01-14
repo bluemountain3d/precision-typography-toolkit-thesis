47edf7f1e2ef637fce3f222b5dc64d6e1f4faf9e
Egil Eskilsson <egil@egileskilsson.se>
2026-01-14 20:48:33 +0100

    feat: integrate content blocks in FontMetricsArticle

--- src/pages/learning/FontMetricsAndWebTypography/FontMetricsArticle.tsx
+++ src/pages/learning/FontMetricsAndWebTypography/FontMetricsArticle.tsx
@@ -1,7 +1,20 @@
+import { Container } from '@/components/layout/Container';
+import { PageSection } from '@/components/layout/PageSection';
+import { ArticleIntroBlock } from './ArticleIntroBlock';
+import { Flex } from '@/components/layout/Flex';
+import { useMediaQuery } from '@/hooks';
+import { queries } from '@/types';
+
 export const FontMetricsArticle = () => {
+  const isBreakpoint = useMediaQuery(queries.isTabletLargeAndUp);
+
   return (
-    <>
-      <p>Font Metrics Article Page</p>
-    </>
+    <PageSection as="section">
+      <Container variant={isBreakpoint ? 'narrow' : 'boxed'}>
+        <Flex direction="column" gap="2xl">
+          <ArticleIntroBlock />
+        </Flex>
+      </Container>
+    </PageSection>
   );
 };
