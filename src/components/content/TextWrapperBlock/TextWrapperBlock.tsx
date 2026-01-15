2192877b7f63206bdecc5b59072df4995ba7c543
Egil Eskilsson <egil@egileskilsson.se>
2026-01-15 17:46:18 +0100

    feat: add TextWrapperBlock component for text wrapping around media
    - Implements float-based layout for natural text wrapping
    - Supports left/right media placement with breakpoint control
    - Responsive stacking on mobile with configurable breakpoints
    - Custom breakout margins (-10% mobile, -20% tablet-lg+)
    - SVG height constraints to respect mediaHeight prop
    - Integrates with TextBox for consistent typography
    - Overrides Text component styles to enable float wrapping
    
    Known limitation: Vertical alignment (mediaAlign prop) not functional with float layout

--- src/components/content/TextWrapperBlock/TextWrapperBlock.tsx
+++ src/components/content/TextWrapperBlock/TextWrapperBlock.tsx
@@ -1,16 +1,69 @@
+import { useMediaQuery } from '@/hooks';
+import { queries, type TextSize } from '@/types';
 import styles from './TextWrapperBlock.module.scss';
 import classNames from 'clsx';
+import { TextBox } from '@/components/typography/TextBox';
 
 interface TextWrapperBlockProps {
-  
+  leftMedia?: React.ReactNode;
+  rightMedia?: React.ReactNode;
+  children: React.ReactNode;
+  mediaHeight?: string; // '100px', '6rem', '5lh', '20ch'
+  breakpoint:
+    | 'isPhabletAndUp'
+    | 'isTabletAndUp'
+    | 'isTabletLargeAndUp'
+    | 'isLaptopAndUp'
+    | 'isDesktopAndUp';
+  textBoxWidthSize?: TextSize;
+  textBoxFlow?: 'em' | 'space';
 }
 
-export const TextWrapperBlock = ({  }: TextWrapperBlockProps) => {
+export const TextWrapperBlock = ({
+  leftMedia,
+  rightMedia,
+  children,
+  mediaHeight = '200px',
+  breakpoint,
+  textBoxWidthSize = 'base',
+  textBoxFlow = 'em',
+}: TextWrapperBlockProps) => {
+  const isBreakpoint = useMediaQuery(queries[breakpoint]);
+  const mediaStyle = {
+    '--media-height': mediaHeight,
+  } as React.CSSProperties;
+
   return (
-    <div className={classNames(
-      styles.textWrapperBlock,
-    )}>
-      
-    </div>
+    <TextBox
+      widthSize={textBoxWidthSize}
+      flow={textBoxFlow}
+      className={classNames(
+        styles['text-wrapper-block'],
+        isBreakpoint && styles[`text-wrapper-block--breakpoint`]
+      )}
+      style={mediaStyle}
+    >
+      {leftMedia && (
+        <div
+          className={classNames(
+            styles['text-wrapper-block__media'],
+            styles['text-wrapper-block__media--left']
+          )}
+        >
+          {leftMedia}
+        </div>
+      )}
+      {rightMedia && (
+        <div
+          className={classNames(
+            styles['text-wrapper-block__media'],
+            styles['text-wrapper-block__media--right']
+          )}
+        >
+          {rightMedia}
+        </div>
+      )}
+      {children}
+    </TextBox>
   );
 };
